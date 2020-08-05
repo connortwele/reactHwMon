183f07a77142a62ae1af99bf822068810927115b
Connor Twele <connortwele@gmail.com>
2020-08-04 21:44:19 -0400

    tuesday homework

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import axios from 'axios'
+import {Link} from "react-router-dom"
+import Pokeball from '../pokeball.png'
+
 
 class Home extends Component{
     state = {
@@ -16,8 +19,11 @@ render(){
     const postList = posts.length ? (posts.map(post=>{
         return (
             <div className="post card" key={post.id}>
+                <img src={ Pokeball } alt="pokeball"/>
                 <div className ="card-content">
-                    <span className="card-title">{post.title}</span>
+                    <Link to= {'/' + post.id}>
+                        <span className="card-title">{post.title}</span>
+                    </Link>
                     <p>{post.body}</p>
                 </div>
             </div>
@@ -26,7 +32,7 @@ render(){
         <div className="center"> No posts yet </div>
     )
     return(
-        <div className="container">
+        <div className="container home">
             <h4 className="center">Home Page</h4>
             <p>{postList}</p>
         </div>
